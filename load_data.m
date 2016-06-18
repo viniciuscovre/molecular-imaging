b2af963aee4c2189bc22bc51bf066222b0c6f591
vinacovre <viniassis@me.com>
2016-06-18 14:26:24 -0500

    loading data code working on mac and with mat files

--- load_data.m
+++
@@ -1,9 +0,0 @@
-[fname dname] = uigetfile('.mat','Select info.mat File');
-
-cd(dname(1:end-1));
-
-load('info.mat');
-
-if type == 'Pearl'
-    [images700,images800,imagesWhite] = script_pearl_data(dname);
-end
--- load_experiment.m
+++ load_experiment.m
@@ -1,11 +1,42 @@
 %Select the text file with the experiment information
 
 
-[fname dname] = uigetfile('.txt','Select the Experiment File');
+[fname dname] = uigetfile({'*.txt;*.rtf','Text Files (*.txt,*.rtf)'; '*.mat','MAT-files (*.mat)'},'Select the Experiment File');
 
-file= fopen(fullfile(dname,fname));
-[information,value]= textscan(file,'%s %s');
+file = fopen(fullfile(dname,fname));
+[information,value] = textscan(file,'%s %s');
 
-if(char(information{1,2}) == 'Pearl')
-    [images700,images800,imagesWhite]=script_pearl_data(dname);
+% Split file name and file type
+[fname,ftype] = strtok(fname,'.');
+
+if(strcmp(ftype,'.mat')) % If user opens a .mat file
+    
+    load(strcat(fname,'.mat'));
+    switch lower(type)
+        case 'pearl'
+            [images700,images800,imagesWhite] = script_pearl_data(dname);
+        otherwise
+            h = msgbox('Invalid Type', 'Error','error');
+            quit
+    end
+    
+else % If user opens a text file
+    
+    % Code for getting the type in the text file for each OS
+    if isunix
+        type = information{1,2}{7,1}(1:end-1);
+    elseif ispc
+        type = char(information{1,2});
+    else
+        disp('Platform not supported')
+    end
+    
+    switch lower(type)
+        case 'pearl'
+            [images700,images800,imagesWhite]=script_pearl_data(dname);
+        otherwise
+            h = msgbox('Invalid Type', 'Error','error');
+            quit
+    end
+    
 end
